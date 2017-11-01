1e492f2ed6200655aac804b83964906e1176c415
Easwari Ramani <eshuprad@gmail.com>
2017-11-01 18:36:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(arrSingers,arrInstru)
+{
+  
+}
+function johnLennonFacts(arrFact)
+{
+
+  return arrFact;
+}
+function iLoveTheBeatles(num)
+{
+  var msg=[];
+  do{
+    msg.assign({},"I love the Beatles!");
+    
+  }
+}
